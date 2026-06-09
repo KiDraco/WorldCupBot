961ba07771f8655feb0a95f4918345065c37ac03
KiDra <lea.maquina@gmail.com>
2026-06-09 17:53:25 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,17 +1,1 @@
-const { Telegraf, Markup } = require('telegraf');
-
-const bot = new Telegraf('process.env.BOT_TOKEN)');
-
-bot.start((ctx) => {
-  ctx.reply(
-    '🏆 Manager Album WorldCup 2026',
-    Markup.inlineKeyboard([
-      Markup.button.webApp(
-        '📘 Abrir Álbum',
-        'https://gestoralbummundial2026.netlify.app/'
-      )
-    ])
-  );
-});
-
-bot.launch();
+console.log("🔥 EL BOT ESTÁ EJECUTANDO ESTE ARCHIVO");
