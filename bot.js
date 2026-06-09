cae7ea34dccbe789b5c8dc419fafc8c1bbe8dd25
KiDra <lea.maquina@gmail.com>
2026-06-09 18:08:20 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,17 @@
-console.log("PROBANDO TOKEN...");
+const { Telegraf, Markup } = require('telegraf');
 
-fetch(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/getMe`)
-  .then(r => r.json())
-  .then(res => console.log("RESULTADO GETME:", res))
-  .catch(err => console.log("ERROR:", err));
+const bot = new Telegraf('process.env.BOT_TOKEN)');
+
+bot.start((ctx) => {
+  ctx.reply(
+    '🏆 Manager Album WorldCup 2026',
+    Markup.inlineKeyboard([
+      Markup.button.webApp(
+        '📘 Abrir Álbum',
+        'https://gestoralbummundial2026.netlify.app/'
+      )
+    ])
+  );
+});
+
+bot.launch();
